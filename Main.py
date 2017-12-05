58f49c8acdab49a9fe8ea361cfec38edc1ef49db
jiangtao <evane1890@gmail.com>
2017-12-05 17:45:18 +0800

    init

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def connectdb():
     print('连接到mysql服务器...')
     # 打开数据库连接
     # 用户名:hp, 密码:Hp12345.,用户名和密码需要改成你自己的mysql用户名和密码，并且要创建数据库TESTDB，并在TESTDB数据库中创建好表Student
-    db = MySQLdb.connect("localhost","root","yxpai8848","domain")
+    db = MySQLdb.connect("localhost","root","123456","domain")
     print('连接上了!')
     return db
 
