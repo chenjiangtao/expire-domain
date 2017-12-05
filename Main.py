07b798058ac8bdfe4d39dc5d8ac6b8c550e28bcb
jiangtao <evane1890@gmail.com>
2017-12-05 17:58:42 +0800

    init

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def connectdb():
     print('连接到mysql服务器...')
     # 打开数据库连接
     # 用户名:hp, 密码:Hp12345.,用户名和密码需要改成你自己的mysql用户名和密码，并且要创建数据库TESTDB，并在TESTDB数据库中创建好表Student
-    db = MySQLdb.connect("localhost","root","123456","domain")
+    db = MySQLdb.connect("localhost","root","yxpai8848","domain")
     print('连接上了!')
     return db
 
@@ -50,7 +50,7 @@ def getTopSites(db):
         cursor.execute(sql)
         # 获取所有记录列表
         results = cursor.fetchall()
-        print 'select ok'
+        print '数据库读取100万数据成功'
         return results
     except:
         print "Error: unable to fecth data"
@@ -69,7 +69,7 @@ def getexpire(topSites):
     print "response get sucess"
     contents = response.content
     tmplist = contents.split('\r\n')
-    print strDate," expired site ",len(tmplist)
+    print strDate,"have",len(tmplist),"site expired,正在比对中。。。"
     sites =["one"]
     goodOne = ["one"]
     for site in tmplist:
